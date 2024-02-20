660b75f7e5362fa3114f7accfa7f54c34d44a962
Ardi <aardithaqi@gmail.com>
2024-02-20 15:57:17 +0100

    Authentication test case added

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
+require("dotenv").config();
 
 module.exports = defineConfig({
+  env: {
+    ...process.env,
+  },
   e2e: {
     specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
--- cypress/e2e/authentication.spec.js
+++ cypress/e2e/authentication.spec.js
@@ -1,11 +1,65 @@
-describe('Tests the registration of a user, login in with the correct and wrong iser details and logging out', ()=>{
-    it('Register User',()=>{
-        cy.visit('https://www.automationexercise.com/')
-        cy.location('pathname').should('equal', '/')
-        cy.get('[class="nav navbar-nav"]').within(()=>{
-            cy.get('li').contains(/ Signup/i).click();
-        })
-        cy.location('pathname').should('equal', '/login')
+describe('User Registration and Account Management', () => {
+    const email = Cypress.env('email');
+    const password = Cypress.env('password');
 
-    })
-})
+    it('Registers a new user, logs in, deletes the account, and logs out', () => {
+        // Load user details from a JSON fixture file
+        cy.fixture('userDetails.json').then((userDetails) => {
+            // Visit the website under test
+            cy.visit('https://www.automationexercise.com/');
+            cy.location('pathname').should('equal', '/');
+
+            // Navigate to the signup page
+            cy.get('[class="nav navbar-nav"]').contains(/Signup/i).click();
+            cy.location('pathname').should('equal', '/login');
+            cy.contains(/New User Signup!/i).should('be.visible');
+
+            // Fill in the signup form
+            cy.getDataQa('signup-name').type(userDetails.username);
+            cy.getDataQa('signup-email').type(email);
+            cy.getDataQa('signup-button').click();
+
+            // Ensure that the "ENTER ACCOUNT INFORMATION" text is visible on the page
+            cy.contains(/ENTER ACCOUNT INFORMATION/i).should('be.visible');
+
+            // Fill in the account information form
+            cy.get('.login-form').within(() => {
+                cy.getDataQa('title').invoke('attr', 'value').contains(/Mr./i).click();
+                cy.getDataQa('name').invoke('attr', 'value').should('contain', userDetails.username);
+                cy.getDataQa('email').invoke('attr', 'value').should('contain', email);
+                cy.getDataQa('password').type(password);
+                cy.getDataQa('days').select(userDetails.day);
+                cy.getDataQa('months').select(userDetails.month);
+                cy.getDataQa('years').select(userDetails.year);
+
+                //Select checkboxes 
+                cy.get('#uniform-newsletter').find('[type="checkbox"]').check({ force: true });
+                cy.get('#uniform-optin').find('[type="checkbox"]').check({ force: true });
+
+                //Fill in the rest of the informations
+                cy.getDataQa('first_name').type(userDetails.first_name);
+                cy.getDataQa('last_name').type(userDetails.last_name);
+                cy.getDataQa('company').type(userDetails.company);
+                cy.getDataQa('address').type(userDetails.address);
+                cy.getDataQa('country').select(userDetails.country);
+                cy.getDataQa('state').type(userDetails.state);
+                cy.getDataQa('city').type(userDetails.city);
+                cy.getDataQa('zipcode').type(userDetails.zipcode);
+                cy.getDataQa('mobile_number').type(userDetails.phoneNumber);
+                cy.getDataQa('create-account').click();
+            });
+
+            // Ensure successful account creation
+            cy.contains(/Account Created!/i).should('be.visible');
+            cy.getDataQa('continue-button').click();
+
+            // Log out the user
+            cy.contains(`Logged in as ${userDetails.username}`);
+
+            cy.get('[class="nav navbar-nav"]').contains(/Delete Account/i).click();
+            // Ensure that the "Account Deleted!" text is visible on the page
+            cy.contains(/Account Deleted!/i).should('be.visible');
+            cy.getDataQa('continue-button').click();
+        });
+    });
+});
