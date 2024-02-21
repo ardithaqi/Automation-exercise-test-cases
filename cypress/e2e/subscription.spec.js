5b47f58ae3eaaf17854160549561a2706ed69677
Ardi <aardithaqi@gmail.com>
2024-02-21 20:43:56 +0100

    added new test case for verifing subscription at the cart page

--- cypress/e2e/subscription.spec.js
+++ cypress/e2e/subscription.spec.js
@@ -1,8 +1,11 @@
 describe('Verify subscription', ()=>{
-    it('Verify subscription in home page',()=>{
+
+    beforeEach('Navigate to automationexercie.com site and validate', ()=>{
         cy.visit('http://automationexercise.com')
         cy.location('pathname').should('equal', '/')
-
+    })
+    it('Verify subscription in home page',()=>{
+        
         cy.get('footer').scrollIntoView();
         cy.contains(/Subscription/i).should('be.visible')
         cy.get('form').then((form)=>{
@@ -11,6 +14,20 @@ describe('Verify subscription', ()=>{
         })
 
         cy.get('[class="alert-success alert"]').invoke('text').should('equal', 'You have been successfully subscribed!')
+    })
+
+    it('Verify subscription in cart page', ()=>{
+
+        cy.get('[class="nav navbar-nav"]').find('li').contains(/Cart/i).click();
+
+        cy.get('footer').scrollIntoView();
+        cy.contains(/Subscription/i).should('be.visible')
+        cy.get('form').then((form)=>{
+            cy.wrap(form).find('[type="email"]').type('arditest@gmail.com')
+            cy.wrap(form).find('button').click();
+        })
+
+        cy.contains(/You have been successfully subscribed!/i).should('be.visible')
     })
 
     
